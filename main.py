fc85bc0cb0615d4c31d16995667fa957732db93a
Jocia276 <casianaapostol10@gmail.com>
2021-10-05 13:23:41 +0300

    Am implementat problema 12 si meniul

--- main.py
+++ main.py
@@ -1,25 +1,62 @@
-def get_leap_years(start, end) :
+def main ():
+    while True:
+        print("1. Ani bisecti.")
+        print("2. Patrate perfecte.")
+        print("x. Iesire din program - exit.")
+        optiune = input ("Alege optiunea: ")
+        if optiune == "1":
+            def get_leap_years(start, end):
 
-    """
-    Afiseaza toti anii bisecti intre doi ani dati(inclusiv anii dati)
+                """
+                Afiseaza toti anii bisecti intre doi ani dati(inclusiv anii dati)
 
-     input : start, end -> int
-     output : anii bisecti dintre start si end -> list[int]
+                 input : start, end -> int
+                 output : anii bisecti dintre start si end -> list[int]
 
-     """
-    list = []
+                 """
+                list = []
 
-    if start<end :
-        print ("Anii bisecti intre " + str(start) + " si " + str(end) + " sunt: ")
+                if start < end:
+                    print("Anii bisecti intre " + str(start) + " si " + str(end) + " sunt: ")
 
-    while start<=end :
-         if (start % 4 == 0) and (start % 100 != 0) or (start % 100 ==0 ) and (start % 400 ==0) :
-            list.append (int(start))
-         start=start+1
+                while start <= end:
+                    if (start % 4 == 0) and (start % 100 != 0) or (start % 100 == 0) and (start % 400 == 0):
+                        list.append(int(start))
+                    start = start + 1
 
-    return list
+                return list
 
-start = int(input("Dati primul an: "))
-end = int(input("Dati ultimul an: "))
+            start = int(input("Dati primul an: "))
+            end = int(input("Dati ultimul an: "))
 
-print(get_leap_years(start, end))
+            print(get_leap_years(start, end))
+        elif optiune == "2":
+            def get_perfect_squares(start, end):
+
+                """
+                  Afiseaza toate patratele perfecte intr-un interval inchis dat.
+
+                    input : start, end -> int
+                    output : numerele patrate perfecte intr start si end -> list[int]
+
+                """
+
+                list = []
+                for i in range (start, end+1):
+                    j=1
+                    while j*j <= i:
+                        if j*j == i:
+                            list.append (i)
+                        j=j+1
+                    i=i+1
+                return list
+
+            start = int(input("Introduceti primul numar: "))
+            end = int(input("Introduceti al doilea numar: "))
+
+            print(get_perfect_squares(start, end))
+
+        else :
+            if optiune == "x":
+                break
+main()
