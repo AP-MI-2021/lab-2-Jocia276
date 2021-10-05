7a6a06b059bc36c554dbd05c32e5f78e38dfaf8e
Jocia276 <casianaapostol10@gmail.com>
2021-10-05 23:40:09 +0300

    Reorganizare cod + implementare functie de test

--- main.py
+++ main.py
@@ -1,62 +1,99 @@
-def main ():
+def main():
     while True:
         print("1. Ani bisecti.")
         print("2. Patrate perfecte.")
         print("x. Iesire din program - exit.")
-        optiune = input ("Alege optiunea: ")
+        optiune = input("Alege optiunea: ")
+
         if optiune == "1":
-            def get_leap_years(start, end):
-
-                """
-                Afiseaza toti anii bisecti intre doi ani dati(inclusiv anii dati)
-
-                 input : start, end -> int
-                 output : anii bisecti dintre start si end -> list[int]
-
-                 """
-                list = []
-
-                if start < end:
-                    print("Anii bisecti intre " + str(start) + " si " + str(end) + " sunt: ")
-
-                while start <= end:
-                    if (start % 4 == 0) and (start % 100 != 0) or (start % 100 == 0) and (start % 400 == 0):
-                        list.append(int(start))
-                    start = start + 1
-
-                return list
-
             start = int(input("Dati primul an: "))
             end = int(input("Dati ultimul an: "))
+            leap_year = get_leap_years(start, end)
+            if leap_year == []:
+                print("Pentru a se putea rula programul, primul an introdus trebuie sa fie mai mic decat al doilea!")
+            else:
+                print("Anii bisecti intre "+ str(start) + " si " + str(end) + " sunt:", leap_year)
 
-            print(get_leap_years(start, end))
         elif optiune == "2":
-            def get_perfect_squares(start, end):
-
-                """
-                  Afiseaza toate patratele perfecte intr-un interval inchis dat.
-
-                    input : start, end -> int
-                    output : numerele patrate perfecte intr start si end -> list[int]
-
-                """
-
-                list = []
-                for i in range (start, end+1):
-                    j=1
-                    while j*j <= i:
-                        if j*j == i:
-                            list.append (i)
-                        j=j+1
-                    i=i+1
-                return list
-
             start = int(input("Introduceti primul numar: "))
             end = int(input("Introduceti al doilea numar: "))
 
-            print(get_perfect_squares(start, end))
+            perfect_squares = get_perfect_squares(start, end)
 
-        else :
-            if optiune == "x":
+            if perfect_squares == []:
+                print("Nu exista patrate perfecte intre numerele date")
+            else:
+                print("Patratele perfecte sunt:",perfect_squares)
+
+        elif optiune == "x":
                 break
+        else:
+            print("Optiune invalida!")
+
+
+def get_leap_years(start, end):
+        """
+        Afiseaza toti anii bisecti intre doi ani dati(inclusiv anii dati)
+
+        input : start, end -> int
+        output : anii bisecti dintre start si end -> list[int]
+
+        """
+        list = []
+
+        if (start > end) or (start == 0) and (end == 0):
+            return list
+
+        while start <= end:
+            if (start % 4 == 0) and (start % 100 != 0) or (start % 100 == 0) and (start % 400 == 0):
+                list.append(int(start))
+            start = start + 1
+
+        return list
+
+
+def get_perfect_squares(start, end):
+    """
+      Afiseaza toate patratele perfecte intr-un interval inchis dat.
+
+        input : start, end -> int
+        output : numerele patrate perfecte intre start si end -> list[int]
+
+    """
+
+    list = []
+    for i in range(start, end + 1):
+        j = 1
+        while j * j <= i:
+            if j * j == i:
+                list.append(i)
+            j = j + 1
+        i = i + 1
+    return list
+
+
+def test_get_leap_years():
+
+
+    assert get_leap_years(2000, 2008) == [2000, 2004, 2008]
+    assert get_leap_years(2001, 2021) == [2004, 2008, 2012, 2016, 2020]
+    assert get_leap_years(1990, 2005) == [1992, 1996, 2000, 2004]
+    assert get_leap_years(2005, 2005) == []
+    assert get_leap_years(2009, 2008) == []
+
+
+test_get_leap_years()
+
+def test_get_perfect_squares():
+
+
+    assert get_perfect_squares(3, 9) == [4, 9]
+    assert get_perfect_squares(3, 3) == []
+    assert get_perfect_squares(12, 36) == [16, 25, 36]
+    assert get_perfect_squares(10, 9) == []
+    assert get_perfect_squares(2, 100) == [4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+test_get_perfect_squares()
+
 main()
+
