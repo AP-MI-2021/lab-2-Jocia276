50f5300ede6629d0bad0a16100a1a79f2633eb24
Jocia276 <casianaapostol10@gmail.com>
2021-10-06 14:00:51 +0300

    Am implementat problema 13

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     while True:
         print("1. Ani bisecti.")
         print("2. Patrate perfecte.")
+        print("3. Transforma temperatura.")
         print("x. Iesire din program - exit.")
         optiune = input("Alege optiunea: ")
 
@@ -12,7 +13,7 @@ def main():
             if leap_year == []:
                 print("Pentru a se putea rula programul, primul an introdus trebuie sa fie mai mic decat al doilea!")
             else:
-                print("Anii bisecti intre "+ str(start) + " si " + str(end) + " sunt:", leap_year)
+                print("Anii bisecti intre " + str(start) + " si " + str(end) + " sunt:", leap_year)
 
         elif optiune == "2":
             start = int(input("Introduceti primul numar: "))
@@ -24,6 +25,12 @@ def main():
                 print("Nu exista patrate perfecte intre numerele date")
             else:
                 print("Patratele perfecte sunt:",perfect_squares)
+
+        elif optiune == "3":
+            temp = float(input("Introduceti temperatura pe care doriti sa o transformati: "))
+            From = str(input("Introduceti scara initiala: "))
+            to = str(input("Introducti scara in care doriti sa transformati temperatura: "))
+            print("Valoarea temperaturii dupa transformare este: ", "{:.2f}".format(get_temp(temp, From, to)))
 
         elif optiune == "x":
                 break
@@ -72,6 +79,32 @@ def get_perfect_squares(start, end):
     return list
 
 
+def get_temp(temp, From, to):
+    """
+    Transformă o temperatură dată într-o scară dată (`K`, `F` sau `C`) într-o altă scară dată.
+    :param temp: Valoarea temperaturii care va fi transformata
+    :param From: Scara in care este temperatura
+    :param to: Scara in care se va transforma temperatura
+    :return: Valoarea temperaturii dupa transformare
+    """
+
+    if (From == 'C') and (to == 'K'):
+        return temp + 273.15
+    if (From == 'C') and (to == 'F'):
+        return temp*(9/5)+32
+
+    if (From == 'K') and (to == 'C'):
+        return temp - 273.15
+    if (From == 'K') and (to == 'F'):
+        return (temp - 273.15) * (9/5) + 32
+
+    if (From == 'F') and (to == 'C'):
+        return (temp - 32) * (5/9)
+    if (From == 'F') and (to == 'K'):
+        return (temp - 32) * (5/9) + 273.15
+
+
+
 def test_get_leap_years():
 
 
@@ -94,6 +127,19 @@ def test_get_perfect_squares():
     assert get_perfect_squares(2, 100) == [4, 9, 16, 25, 36, 49, 64, 81, 100]
 
 test_get_perfect_squares()
+
+
+def test_get_temp():
+
+    assert "{:.2f}".format(get_temp(100, 'C', 'K')) == '373.15'
+    assert "{:.2f}".format(get_temp(56, 'C', 'F')) == '132.80'
+    assert "{:.2f}".format(get_temp(629, 'K', 'C')) == '355.85'
+    assert "{:.2f}".format(get_temp(120, 'K', 'F')) == '-243.67'
+    assert "{:.2f}".format(get_temp(566, 'F', 'C')) == '296.67'
+    assert "{:.2f}".format(get_temp(120, 'F', 'K')) == '322.04'
+
+test_get_temp()
+
 
 main()
 
